1a84d42a8848a7d8e38d77ccc48a8759fb738dc9
dorjizangpo067 <dorjizangpo067@gmail.com>
2026-01-09 10:44:35 +0600

    make structure and written simple fastapi

--- app/main.py
+++ app/main.py
@@ -1,84 +1,52 @@
-from typing import Annotated
+from fastapi import Depends, FastAPI
 
-from fastapi import FastAPI, Depends, HTTPException
-from sqlmodel import SQLModel, create_engine, Field, Session, select
-from pydantic import BaseModel
+from .dependencies import get_query_token, get_token_header
+from .internal import admin
+from .routers import items, users
+description = """
+ChimichangApp API helps you do awesome stuff. 🚀
 
-class HeroBase(SQLModel):
-    name: str = Field(index=True)
-    age: int | None = None
+## Items
 
-class Hero(HeroBase, table = True):
-    id: int | None = Field(default=None, primary_key=True)
-    secret_name: str
+You can **read items**.
 
-class HeroPublic(HeroBase):
-    id: int
+## Users
 
-class HeroCreate(HeroBase):
-    secret_name: str
+You will be able to:
 
-class HeroUpdate(HeroBase):
-    name: str | None = None
-    age: int | None = None
-    secret_name: str | None = None
-
-
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-
-engine = create_engine(url=sqlite_url, connect_args={"check_same_thread": False})
-
-def create_db_and_tabble():
-    SQLModel.metadata.create_all(engine)
-
-def get_session():
-    with Session(engine) as session:
-        yield session
+* **Create users** (_not implemented_).
+* **Read users** (_not implemented_).
+"""
+app = FastAPI(
+        title="ChimichangApp",
+        description=description,
+        summary="Deadpool's favorite app. Nuff said.",
+        version="0.0.1",
+        terms_of_service="http://example.com/terms/",
+        contact={
+            "name": "Deadpoolio the Amazing",
+            "url": "http://x-force.example.com/contact/",
+            "email": "dp@x-force.example.com",
+        },
+        license_info={
+            "name": "Apache 2.0",
+            "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+        },
+        dependencies=[Depends(get_query_token)]
+    )
 
 
-app = FastAPI()
+app.include_router(users.router)
+app.include_router(items.router)
+app.include_router(
+    admin.router,
+    prefix="/admin",
+    tags=["admin"],
+    dependencies=[Depends(get_token_header)],
+    responses={418: {"description": "I'm a teapot"}},
+)
 
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tabble()
 
-@app.post("/heros/", response_model=HeroPublic)
-def create_hero(hero: HeroCreate, session: Annotated[Session, Depends(get_session)]):
-    hero = Hero.model_validate(hero)
-    session.add(hero)
-    session.commit()
-    session.refresh(hero)
-    return hero
-
-@app.get("/heros/all", response_model=list[HeroPublic])
-async def get_all_heros(session: Annotated[Session, Depends(get_session)], offset: int = 0, limit: int = 2) -> list[Hero]:
-    heroes = session.exec(
-        select(Hero).offset(offset).limit(limit)
-    ).all()
-    return heroes
-
-@app.get("/heros/{hero_id}", response_model=HeroPublic)
-async def get_one_hero(hero_id: int, session: Annotated[Session, Depends(get_session)]) -> Hero:
-    hero = session.get(Hero, hero_id)
-    if not hero:
-        raise HTTPException(status_code=404, detail="Hero not found")
-    return hero
-
-@app.delete("/heros/{hero_id}")
-def delet_hero(hero_id: int, session: Annotated[Session, Depends(get_session)]) -> dict:
-    hero = session.get(Hero, hero_id)
-    session.delete(hero)
-    session.commit()
-    return {"message": "Hero Deleted"}
-
-@app.patch("/heros/{hero_id}", response_model=HeroPublic)
-async def update_hero(hero_id: int, form: HeroUpdate, session: Annotated[Session, Depends(get_session)]) -> Hero:
-    hero = session.get(Hero, hero_id)
-    if not hero:
-        raise HTTPException(status_code=404, detail="Hero not found")
-    hero_data = form.model_dump(exclude_unset=True)
-    hero.sqlmodel_update(hero_data)
-    session.commit()
-    session.refresh(hero)
-    return hero
+@app.get("/")
+async def root():
+    return {"message": "Hello Bigger Applications!"}
